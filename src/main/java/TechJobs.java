1266247cb571d89ec224e050535f20c7ca496785
adamstgr <adam.stgr@gmail.com>
2022-07-29 17:09:43 -0400

    Prints jobs in alignment with the directions

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+            return;
+        }
+        for (HashMap<String, String> job: someJobs){
+            String jobListing = "\n*****\n";
+            for(Map.Entry<String, String> data: job.entrySet() ) {
+                    jobListing += data.getKey() + ": " + data.getValue() + "\n";
+            }
+            jobListing += "*****\n";
+            System.out.printf(jobListing);
+        }
     }
 }
